2e0562f8fa82e360d93962d2a84963e94ef0ad72
Jey Chhun <136158401+chanhengchhun@users.noreply.github.com>
2026-07-09 22:58:43 -0400

    add real-time messaging using SingleR

--- Program.cs
+++ Program.cs
@@ -2,6 +2,7 @@ var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 builder.Services.AddRazorPages();
+builder.Services.AddSignalR();
 
 var app = builder.Build();
 
@@ -22,5 +23,7 @@ app.UseAuthorization();
 app.MapStaticAssets();
 app.MapRazorPages()
    .WithStaticAssets();
+
+app.MapHub<MessageHub>("/MessageHub");
 
 app.Run();
--- wwwroot/js/site.js
+++ wwwroot/js/site.js
@@ -1,4 +1,35 @@
-﻿// Please see documentation at https://learn.microsoft.com/aspnet/core/client-side/bundling-and-minification
+// Please see documentation at https://learn.microsoft.com/aspnet/core/client-side/bundling-and-minification
 // for details on configuring this project to bundle and minify static web assets.
 
 // Write your JavaScript code.
+
+var sendButton = document.getElementById("send");
+var messageInput = document.getElementById("messageInput");
+var newMessageDiv = document.getElementById("newMessage");
+
+if (sendButton && messageInput && newMessageDiv) {
+    var connection = new signalR.HubConnectionBuilder()
+        .withUrl("/MessageHub")
+        .build();
+
+    connection.on("ReceiveNewMessage", function (message) {
+        let paragraph = document.createElement("p");
+        paragraph.innerText = message;
+        newMessageDiv.appendChild(paragraph);
+    });
+
+    connection.start().catch(function (err) {
+        return console.error(err.toString());
+    });
+
+    sendButton.addEventListener("click", function (event) {
+        let message = messageInput.value;
+        if (message) {
+            connection.invoke("PostMessage", message).catch(function (err) {
+                return console.error(err.toString());
+            });
+            messageInput.value = "";
+        }
+        event.preventDefault();
+    });
+}
